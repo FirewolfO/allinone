a3db387bb46b093fb01bdaac12ed41510256d07f
liuxing <liuxing@megvii.com>
2020-03-10 16:09:55 +0800

    1. add some comment

--- codes/lx-parent/lx-common/src/main/java/com/firewolf/lx/tools/email/entity/Email.java
+++ codes/lx-parent/lx-common/src/main/java/com/firewolf/lx/tools/email/entity/Email.java
@@ -1,5 +1,7 @@
 package com.firewolf.lx.tools.email.entity;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,12 +10,13 @@ import org.springframework.web.multipart.MultipartFile;
  * Date: 2020/3/10 11:50
  */
 @Data
+@ApiModel("邮件信息")
 public class Email {
-
-    private String from;
-    private String fromPassword;
+    @ApiModelProperty("接受方邮件")
     private String to;
+    @ApiModelProperty("主题")
     private String title;
+    @ApiModelProperty("内容")
     private String content;
     private MultipartFile[] attenchments;
 }
