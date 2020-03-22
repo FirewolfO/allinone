84782f9813d89df0013063b1875314f1af722e17
liuxing <liuxing@megvii.com>
2020-03-22 19:50:14 +0800

    1. junit测试用例完结

---
+++ codes/all-parent/all-base/src/test/java/com/firewolf/busi/example/RepeatTestDriver.java
@@ -0,0 +1,15 @@
+package com.firewolf.busi.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.TestInfo;
+
+class RepeatTestDriver {
+
+
+    @DisplayName("repeat test")
+    @RepeatedTest(value = 4, name = "执行测试： {displayName}, 第 {currentRepetition} / {totalRepetitions} 次！ ")
+    void testRepeat(TestInfo testInfo) {
+        assert !testInfo.getDisplayName().contains("3");
+    }
+}
