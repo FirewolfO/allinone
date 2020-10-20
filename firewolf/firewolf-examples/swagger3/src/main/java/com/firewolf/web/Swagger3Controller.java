e8f8ee6183ea98b736ff5a847b45334b4b899001
liuxing <1483345163@qq.com>
2020-10-20 18:54:05 +0800

    aaa

--- firewolf/firewolf-examples/swagger3/src/main/java/com/firewolf/web/Swagger3Controller.java
+++ firewolf/firewolf-examples/swagger3/src/main/java/com/firewolf/web/Swagger3Controller.java
@@ -8,6 +8,8 @@ import io.swagger.annotations.ApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 描述：
@@ -16,12 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Api(tags = {"测试项目"})
-public class Swagger3Controller {
+public class Swagger3Controller implements WebMvcConfigurer {
 
 
     @PostMapping
     @ApiOperation(value = "测试方法")
     public Dog findDog(@RequestBody Person person) {
         return Dog.builder().name("TOM").build();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
