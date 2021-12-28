548dcdb22f7c624df69297785d74a9018c562d00
liuxing <1483345163@qq.com>
2021-12-28 21:39:08 +0800

    aaa

--- firewolf/firewolf-leetcode/src/main/java/树/二叉树/二叉树的层序遍历II_107.java
+++ firewolf/firewolf-leetcode/src/main/java/树/二叉树/二叉树的层序遍历II_107.java
@@ -1,44 +1,44 @@
 package 树.二叉树;
 
-import utils.TreeUtils;
-import utils.TreeUtils.*;
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-class 二叉树的层序遍历_102 {
+import utils.TreeUtils;
+import utils.TreeUtils.*;
+
+class 二叉树的层序遍历II_107 {
 
     public static void main(String[] args) {
         TreeNode<Integer> integerTreeNode = TreeUtils.buildBSTTree(3, 9, 20, null, null, 15, 7);
-        List<List<Integer>> lists = new 二叉树的层序遍历_102().levelOrder(integerTreeNode);
+        List<List<Integer>> lists = new 二叉树的层序遍历II_107().levelOrderBottom(integerTreeNode);
         System.out.println(lists);
     }
 
-    public List<List<Integer>> levelOrder(TreeNode<Integer> root) {
+    public List<List<Integer>> levelOrderBottom(TreeNode<Integer> root) {
         List<List<Integer>> res = new ArrayList<>();
         if (root == null) {
             return res;
         }
-        Queue<TreeNode<Integer>> que = new LinkedList<>();
-        que.offer(root);
-        while (!que.isEmpty()) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
             // 遍历某一层
-            int len = que.size();
             List<Integer> oneLevel = new ArrayList<>();
+            int len = queue.size();
             while (len > 0) {
-                TreeNode<Integer> node = que.poll();
+                TreeNode<Integer> node = queue.poll();
                 oneLevel.add(node.val);
-                if (node.left != null) { //加入下一层
-                    que.offer(node.left);
+                if (node.left != null) { // 加入下一层
+                    queue.offer(node.left);
                 }
                 if (node.right != null) {
-                    que.offer(node.right);
+                    queue.offer(node.right);
                 }
                 len--;
             }
-            res.add(oneLevel);
+            res.add(0, oneLevel);
         }
         return res;
     }
