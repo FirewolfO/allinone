eef877a98ccad1e3369f5fac284a33197a4c9e1c
liuxing <1483345163@qq.com>
2022-01-03 21:45:44 +0800

    aaa

--- firewolf/firewolf-leetcode/src/main/java/树/二叉树/翻转二叉树_226.java
+++ firewolf/firewolf-leetcode/src/main/java/树/二叉树/翻转二叉树_226.java
@@ -3,6 +3,7 @@ package 树.二叉树;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 import static utils.TreeUtils.*;
 
@@ -39,7 +40,41 @@ public class 翻转二叉树_226 {
         queue.offer(root);
         while (!queue.isEmpty()) {
             int size = queue.size();
+            while (size > 0) {
+                TreeNode poll = queue.poll();
+                TreeNode tmp = poll.left;
+                poll.left = poll.right;
+                poll.right = tmp;
+                if (poll.left != null) {
+                    queue.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.offer(poll.right);
+                }
+                size--;
+            }
+        }
+        return root;
+    }
 
+    //------------------深度遍历优先----------------
+    public TreeNode invertTreeDST(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+            TreeNode tmp = pop.left;
+            pop.left = pop.right;
+            pop.right = tmp;
+            if (pop.left != null) {
+                stack.push(pop.left);
+            }
+            if (pop.right != null) {
+                stack.push(pop.right);
+            }
         }
         return root;
     }
