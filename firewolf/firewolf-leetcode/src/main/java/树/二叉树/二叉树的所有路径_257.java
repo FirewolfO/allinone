b535e2c2439b76ed89ec5e394d78377d00449c9b
Liuxing <liuxing@megvii.com>
2022-01-06 22:55:12 +0800

    aaa

---
+++ firewolf/firewolf-leetcode/src/main/java/树/二叉树/二叉树的所有路径_257.java
@@ -0,0 +1,36 @@
+package 树.二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.TreeUtils.*;
+
+class 二叉树的所有路径_257 {
+
+    public static void main(String[] args) {
+        TreeNode<Integer> root = buildBSTTree(1, 2, 3, null, 5);
+        System.out.println(new 二叉树的所有路径_257().binaryTreePaths(root));
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            res.add(root.val + "");
+            return res;
+        }
+        if (root.left != null) {
+            for (String one : binaryTreePaths(root.left)) {
+                res.add(root.val + "->" + one);
+            }
+        }
+        if (root.right != null) {
+            for (String one : binaryTreePaths(root.right)) {
+                res.add(root.val + "->" + one);
+            }
+        }
+        return res;
+    }
+}
