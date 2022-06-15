46c7e3334c36a94c2188e687977ea83ba1ce33c0
易小寒O <1483345163@qq.com>
2022-06-15 12:40:40 +0000

    add docs/notes/Docker/softshells/start_mysql.sh.

---
+++ docs/notes/Docker/softshells/start_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$1 -> 版本号；$2 -> 端口号; $3 -> f 强制启动，会把已有的进程删除
+#强制停止
+if [ $3 == "f" ]; then
+       sudo docker stop mysql$1
+       sudo docker rm mysql$1
+fi
+# 启动
+sudo docker run \
+    --name mysql$1 \
+    --restart always \
+    -d \
+    -v $PWD/datadir:/var/lib/mysql \
+    -v $PWD/conf.d:/etc/mysql/conf.d \
+    -e MYSQL_ROOT_PASSWORD="1" \
+    -p $2:3306 mysql:$1 \
+    --general-log=ON \
+    --general-log-file=/var/log/mysql/general-log.log
