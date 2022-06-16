e00f2e9c4e14c832b0fad4318494dfd457f643fd
易小寒O <1483345163@qq.com>
2022-06-16 02:50:55 +0000

    update docs/notes/Docker/softshells/util_setcharacter.sh.

--- docs/notes/Docker/softshells/util_setcharacter.sh
+++ docs/notes/Docker/softshells/util_setcharacter.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-sed -i "s/\r//g" $1
+#需要被重新编码的文件所属目录
+dir=$1
+if [ -z $dir ]; then 
+	dir=$PWD
+fi
+files=`ls -l $dir| grep -v '^d' | awk -F ' ' '{print $9}' | grep '.sh$'`
+echo $files
+for file in $files 
+do
+	echo "change $dir/$file encoding..."
+	sed -i "s/\r//g" $dir/$file
+done 
